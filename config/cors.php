2db8d51586abce3ceb4c28e96d030291da6768d9
ebenezer <ebenezermerd@gmail.com>
2025-01-22 12:11:43 +0300

    project configured to be shared with others

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8080', 'http://api.koricha-ecommerce.com', 'http://localhost:5174'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
