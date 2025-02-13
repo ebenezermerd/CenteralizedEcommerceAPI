ba25db6f5496ef3a1106347df66371881f73d1f8
ebenezer <ebenezermerd@gmail.com>
2025-02-14 01:56:53 +0300

    updated cors to handle the admin frontend side

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie', 'admin/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8080', 'http://localhost:5174'],
+    'allowed_origins' => ['http://localhost:8080', 'http://localhost:5174', 'https://admin.korecha.com.et'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
