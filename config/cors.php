4b12bfdb0628500bddfe8f4adc2580c2836cd1d2
ebenezer <ebenezermerd@gmail.com>
2025-02-16 10:55:57 +0300

    cors configured to handle client front side

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie', 'admin/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8080', 'http://localhost:5173', 'https://admin.korecha.com.et'],
+    'allowed_origins' => ['http://localhost:8080', 'http://localhost:5173', 'https://admin.korecha.com.et', 'https://korecha.com.et'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
