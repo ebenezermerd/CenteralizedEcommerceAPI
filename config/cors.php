4f0b39eeaf196e65fc954a3b12cf4bc8756d2537
ebenezer <ebenezermerd@gmail.com>
2025-02-17 13:53:02 +0300

    added cors for debug localhost

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie', 'admin/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://www.korecha.com.et', 'https://admin.korecha.com.et'],
+    'allowed_origins' => ['https://www.korecha.com.et', 'https://admin.korecha.com.et', 'http://localhost:8080'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
