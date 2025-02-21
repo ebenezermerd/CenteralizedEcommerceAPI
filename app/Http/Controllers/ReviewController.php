79966daa3427229abad8188c22c96894aa475fca
ebenezer <ebenezermerd@gmail.com>
2025-02-21 10:18:40 +0300

    getting the user from the user_id sent added user import

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use App\Models\User;
+
 
 class ReviewController extends Controller
 {
